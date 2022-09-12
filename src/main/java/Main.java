a2267c7f5079e5af38ca9fdff042269d9fc2c3d9
marcusFN <marc599n@stud.kea.dk>
2022-09-12 19:46:15 +0200

    second Commit User Story 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,30 +8,42 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Velkommen til universet med dine superhelte!");
-        System.out.println("indtast navnet på din superhelt:");
-        String SuperHelteNavn = scanner.nextLine();
+        System.out.println("Velkommen Superhero universet!\n1. Opret superhelt\n9. Afslut program");
+        int menuTal = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("indtast det rigtige navn på "+ SuperHelteNavn+":");
-        String SuperHelterigtigeNavn = scanner.nextLine();
+        if (menuTal == 1) {
+            System.out.println("indtast navnet på din superhelt:");
+            String SuperHelteNavn = scanner.nextLine();
 
-        // skal have det her til at virke med boolean
-        System.out.println("indtast om din superhelt er et menneske (true/false):");
-        String SuperHeltMenneske = scanner.nextLine();
+            System.out.println("indtast det rigtige navn på " + SuperHelteNavn + ":");
+            String SuperHelterigtigeNavn = scanner.nextLine();
+
+            // skal have det her til at virke med boolean
+            System.out.println("indtast om din superhelt er et menneske (true/false):");
+            boolean SuperHeltMenneske = scanner.nextBoolean();
+            scanner.nextLine();
+
+            System.out.println("indtast hvilke superkrafter " + SuperHelteNavn + " har:");
+            String SuperHelteKrafter = scanner.nextLine();
+
+            System.out.println("indtast hvilket år din superhelte opstod:");
+            int SuperHelteAlder = scanner.nextInt();
+
+            System.out.println("indtast hvilke styrke din superhelt har (0-100):");
+            int SuperhelteStyrke = scanner.nextInt();
 
 
-        System.out.println("indtast hvilke superkrafter "+ SuperHelteNavn + " har:");
-        String SuperHelteKrafter = scanner.nextLine();
+            System.out.println("Din Superhelt!\nSuperhelte Navn: " + SuperHelteNavn + "\nRigtige navn: " + SuperHelterigtigeNavn + "\nmenneske: " + SuperHeltMenneske + "\nKrafter: " + SuperHelteKrafter + "\nOpstandelse: " + SuperHelteAlder + "\nStyrke: " + SuperhelteStyrke);
+            
 
-        System.out.println("indtast hvilket år din superhelte opstod:");
-        int SuperHelteAlder = scanner.nextInt();
-
-        System.out.println("indtast hvilke styrke din superhelt har:");
-        int SuperhelteStyrke = scanner.nextInt();
-
-
-
-        System.out.println("Din Superhelt!\nSuperhelte Navn: "+SuperHelteNavn+"\nRigtige navn: "+ SuperHelterigtigeNavn+"\nmenneske: "+SuperHeltMenneske+"\nKrafter: "+SuperHelteKrafter+"\nOpstandelse: "+SuperHelteAlder+"\nStyrke: "+SuperhelteStyrke);
-
+        }
+        else if (menuTal == 9){
+            System.out.println("program afsluttes");
+        } else{
+            System.out.println("ugyldigt input");
+        }
+        System.out.println("\n1. Opret superhelt\n9. Afslut program");
+        
     }
 }
