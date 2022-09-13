4b8ce164da6119eea527c243641c681459d32f43
marcusFN <marc599n@stud.kea.dk>
2022-09-13 12:45:28 +0200

    fourth commit user story 3

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,24 +1,20 @@
+import java.util.ArrayList;
+
 public class Database {
-    private Superhero[] superheroes;
-    private int antalSuperheros;
-    private int superHeroesAmount;
 
 
 
-    public Database(){
-        superheroes = new Superhero[5];
-        antalSuperheros = 0;
-        superHeroesAmount = 0;
-    }
+    private ArrayList<Superhero>superheroes = new ArrayList<>();
 
     public void createSuperHero(String heroName, String heroRealName, boolean heroHuman, String heroPowers, int heroCreation, double heroPowerLevel) {
         Superhero newSuperHero = new Superhero(heroName, heroRealName, heroHuman, heroPowers, heroCreation, heroPowerLevel);
-        superheroes[antalSuperheros++] = newSuperHero;
-        superheroes[superHeroesAmount++] = newSuperHero;
+       superheroes.add(newSuperHero);
     }
 
 
-    public void setAntalSuperheros() {
-        this.antalSuperheros = antalSuperheros;
-    }
+
+
+
+
+
 }
